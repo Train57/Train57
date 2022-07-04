423464c58a31dbc5c67f48482db7323c5eb55e49
Train57 <108531067+Train57@users.noreply.github.com>
2022-07-04 19:18:57 +0800

    群轰炸

---
+++ 群轰炸.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <windows.h>
+int main()
+{
+	int Message_Num = 5;
+	FindWindow(NULL, L"苏盐曹淮联合反卷中心");
+
+	while (Message_Num > 0)
+	{
+		SendMessage(FindWindow(NULL, L"苏盐曹淮联合反卷中心"), WM_PASTE, NULL, 0);
+		SendMessage(FindWindow(NULL, L"苏盐曹淮联合反卷中心"), WM_KEYDOWN, VK_RETURN, 0);
+		Message_Num--;
+	}
+
+
+	return 0;
+}
